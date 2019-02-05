0bd6dff57e15a0b0b51c17e1b778f624aece569f
vsilaev <vsilaev@gmail.com>
2019-02-05 16:49:36 +0300

    Identation in module-info

--- net.tascalate.javaflow.providers.asmx/src/main/java/module-info.java
+++ net.tascalate.javaflow.providers.asmx/src/main/java/module-info.java
@@ -1,9 +1,24 @@
-								module net.tascalate.javaflow.providers.asmx {
-								    requires org.objectweb.asm;
-								    requires org.objectweb.asm.tree; 
-								    requires org.objectweb.asm.tree.analysis;
+/**
+ * ﻿Copyright 2013-2019 Valery Silaev (http://vsilaev.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module net.tascalate.javaflow.providers.asmx {
+    requires org.objectweb.asm;
+    requires org.objectweb.asm.tree; 
+    requires org.objectweb.asm.tree.analysis;
 
-								    requires transitive net.tascalate.javaflow.spi;
+    requires transitive net.tascalate.javaflow.spi;
 
-								    exports org.apache.commons.javaflow.providers.asmx;
-								}
+    exports org.apache.commons.javaflow.providers.asmx;
+}
